f777283fb7a31a473873c99330dc67c866fa78c1
Lars, DK7LST <dk7lst@gmx.net>
2016-10-23 01:28:39 +0200

    Calculate SHA256-Hash using OpenSSL.

---
+++ src/lib/data/sha256.cpp
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "sha256.h"
+
+#if OPENSSL_VERSION_NUMBER < 0x010100000 // enable for OpenSSL below v1.1
+  #define EVP_MD_CTX_new() EVP_MD_CTX_create()
+  #define EVP_MD_CTX_free(mdctx) EVP_MD_CTX_destroy(mdctx)
+#endif
+
+bool Sha256::selftest() {
+  Sha256 h;
+  return h.init() && h.put("Hallo", 5) && !strcmp(h.getHashHex().c_str(), "753692EC36ADB4C794C973945EB2A99C1649703EA6F76BF259ABB4FB838E013E");
+}
+
+Sha256::Sha256() {
+  m_mdctx = NULL;
+}
+
+Sha256::~Sha256() {
+  reset();
+}
+
+bool Sha256::init() {
+  reset();
+  m_mdctx = EVP_MD_CTX_new();
+  return m_mdctx && EVP_DigestInit_ex(m_mdctx, EVP_sha256(), NULL) == 1;
+}
+
+bool Sha256::reset() {
+  if(m_mdctx) {
+    EVP_MD_CTX_free(m_mdctx);
+    m_mdctx = NULL;
+  }
+  return true;
+}
+
+bool Sha256::put(const void *pData, int iLength) {
+  return m_mdctx && EVP_DigestUpdate(m_mdctx, pData, iLength) == 1;
+}
+
+std::string Sha256::getHashHex() {
+  std::string sHexStr;
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len = 0;
+  if(m_mdctx && EVP_DigestFinal_ex(m_mdctx, md_value, &md_len) == 1) {
+    char buf[8];
+    for(unsigned i = 0; i < md_len; ++i) {
+      sprintf(buf, "%02X", md_value[i]);
+      sHexStr += buf;
+    }
+  }
+  reset();
+  return sHexStr;
+}
---
+++ src/lib/data/sha256.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+#include <openssl/evp.h>
+
+// Calculate SHA256-Hash using OpenSSL.
+// See https://www.openssl.org/docs/manmaster/crypto/EVP_DigestInit.html for Details.
+class Sha256 {
+public:
+  static bool selftest();
+
+  Sha256();
+  ~Sha256();
+  bool init();
+  bool reset();
+  bool put(const void *pData, int iLength);
+  std::string getHashHex();
+
+protected:
+  EVP_MD_CTX *m_mdctx;
+};
